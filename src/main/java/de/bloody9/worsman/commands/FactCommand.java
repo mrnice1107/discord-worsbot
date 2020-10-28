f03ec588e803b67f0f1e9d03a71f7f359caa595b
mrnice1107 <55033883+mrnice1107@users.noreply.github.com>
2020-10-28 21:05:22 +0100

    prepare for core 2.0.0
    adapt commands to new core version

--- src/main/java/de/bloody9/worsman/commands/FactCommand.java
+++ src/main/java/de/bloody9/worsman/commands/FactCommand.java
@@ -3,15 +3,16 @@ package de.bloody9.worsman.commands;
 import static de.bloody9.core.logging.Logger.*;
 import static de.bloody9.core.helper.PermissionHelper.memberHasPermission;
 
+import de.bloody9.core.exceptions.Command.*;
+import de.bloody9.core.exceptions.Feature.FeatureException;
+import de.bloody9.core.exceptions.Feature.FeatureLoadException;
 import de.bloody9.core.helper.Helper;
 import de.bloody9.core.models.interfaces.BotCommand;
-import de.bloody9.core.Bot;
 import de.bloody9.core.models.objects.PermissionObject;
 import de.bloody9.worsman.features.guildfact.GuildFacts;
 import net.dv8tion.jda.api.entities.*;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.StringJoiner;
 
@@ -25,29 +26,38 @@ public class FactCommand implements BotCommand {
 
     private final String help;
 
+    private final List<String> aliases;
+
+
     public FactCommand() {
-        String prefix = Bot.INSTANCE.getCommandPrefix();
         permissionObjects = new ArrayList<>();
 
         permissionObjects.add(new PermissionObject(generalPermission, "Execute command"));
         permissionObjects.add(new PermissionObject(editPermission, "add/remove/list permissions"));
 
+        aliases = new ArrayList<>();
+
         description = "What this commands says is fact!";
 
-        help = "fact Command\n"
-                + prefix + "fact [add/remove/list]\n" +
-                "add <fact> | adds a new fact\n" +
-                "remove <fact_id> | removes a fact by id (get id from list)\n" +
-                "list | lists the available facts";
+        help = "fact Command\n" +
+                "<prefix> fact [add/remove/list]\n" +
+                "<prefix> fact add <fact> | adds a new fact\n" +
+                "<prefix> fact remove <fact_id> | removes a fact by id (get id from list)\n" +
+                "<prefix> fact list | lists the available facts";
     }
 
     private void sendHelp(User user) {
-        Helper.sendPrivateMessage(user, help);
+        Helper.sendPrivateMessage(user, getHelp());
+    }
+
+    @Override
+    public List<String> getAlias() {
+        return aliases;
     }
 
     @Override
     public String getHelp() {
-        return help;
+        return Helper.constructHelp(help);
     }
 
     @Override
@@ -65,38 +75,34 @@ public class FactCommand implements BotCommand {
 
         debug("start guildCommand");
 
-        debug("command: " + command);
-        debug("sender: " + sender.getName() + ":" + sender.getId());
-        debug("message: " + message.getContentRaw());
-        debug("args: " + Arrays.toString(args));
-
         if (!memberHasPermission(generalPermission, message.getMember())) {
-            return false;
+            throw new NoPermissionCommandException(sender, generalPermission);
         }
 
         GuildFacts facts = GuildFacts.getGuildFactsByGuildID(message.getGuild().getId());
         if (facts == null) {
             error("failed to get GuildFactObject by id");
             Helper.sendPrivateMessage(sender, "An internal error occurred while executing your command!");
-            return false;
+            throw new FeatureLoadException("GuildFacts");
         }
 
         if (args.length == 0) {
             TextChannel channel = message.getTextChannel();
             channel.sendMessage(facts.getRandomFact()).queue();
-            return true;
+            return false;
         }
 
         if (!memberHasPermission(editPermission, message.getMember())) {
-            return false;
+            throw new NoPermissionCommandException(sender, editPermission);
         }
 
         switch (args[0].toLowerCase()) {
             case "add": {
                 if (args.length == 1) {
                     sendHelp(sender);
+                    return true;
                 }
-                StringJoiner  joiner = new StringJoiner(" ");
+                StringJoiner joiner = new StringJoiner(" ");
                 for (int i = 1; i < args.length; i++) {
                     joiner.add(args[i]);
                 }
@@ -107,12 +113,13 @@ public class FactCommand implements BotCommand {
                     return true;
                 } else {
                     Helper.sendPrivateMessage(sender, "An internal error occurred while inserting your fact: " + fact);
-                    return false;
+                    throw new FeatureException("GuildFact failed add fact");
                 }
             }
             case "remove": {
                 if (args.length != 2) {
                     sendHelp(sender);
+                    return true;
                 }
 
                 int id;
@@ -120,7 +127,7 @@ public class FactCommand implements BotCommand {
                     id = Integer.parseInt(args[1]);
                 } catch (NumberFormatException e) {
                     Helper.sendPrivateMessage(sender, "You need to enter a number as id!");
-                    return false;
+                    throw new NoNumberCommandException();
                 }
 
                 if (facts.removeFact(id)) {
@@ -128,7 +135,7 @@ public class FactCommand implements BotCommand {
                     return true;
                 } else {
                     Helper.sendPrivateMessage(sender, "Failed to remove the fact. *" + id + "* is no valid or used id!");
-                    return false;
+                    throw new FeatureException("GuildFact failed remove fact");
                 }
             }
             case "list": {
@@ -144,7 +151,7 @@ public class FactCommand implements BotCommand {
             }
             default: {
                 sendHelp(sender);
-                return false;
+                throw new WrongArgumentCommandException();
             }
         }
     }
